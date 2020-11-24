c70f27a0c392dce72299cc9522c7fe1c1a0afbaa
Your Name <darwinmuthu450@gmail.com>
2020-11-25 04:36:43 +0530

    init

---
+++ src/AccountDetails.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class AccountDetails extends Component {
+  render() {
+    return <div></div>;
+  }
+}
---
+++ src/CompanyDetails.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CompanyDetails extends Component {
+  render() {
+    return <></>;
+  }
+}
